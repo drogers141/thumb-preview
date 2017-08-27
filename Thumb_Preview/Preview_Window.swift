b7a47ba02a13f992dbb7b540c1e70716493ae4f7
Dave Rogers <dave@drogers.us>
2017-08-26 23:03:41 -0700

    initial commit plus clear window

---
+++ Thumb_Preview/Preview_Window.swift
@@ -0,0 +1,28 @@
+//
+//  Preview_Window.swift
+//  Thumb_Preview
+//
+//  Created by David Rogers on 8/16/17.
+//  Copyright © 2017 David Rogers. All rights reserved.
+//
+
+import Cocoa
+
+class Preview_Window: NSWindow {
+
+    override init(contentRect: NSRect, styleMask style: NSWindowStyleMask,
+                  backing bufferingType: NSBackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect, styleMask: style, backing: bufferingType, defer: flag)
+
+        // Set the opaque value off,remove shadows and fill the window with clear (transparent)
+        self.isOpaque = false
+        self.hasShadow = false
+        self.backgroundColor = NSColor.clear
+
+        // Change the title bar appereance
+        self.title = "Thumb Preview"
+        //self.titleVisibility = .hidden
+        self.titlebarAppearsTransparent = true
+    }
+}
+
